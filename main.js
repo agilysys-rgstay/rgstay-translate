a69915528962521a78a9a251ec5f253b1e56a608
agilysys-rgstay <69390909+agilysys-rgstay@users.noreply.github.com>
2020-12-01 19:33:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-const GOOGLE_TRANSLATOR = require('@danke77/google-translate-api');
+const axios = require('axios');
 const EXPRESS = require('express');
 const bodyParser = require('body-parser');
 const app = EXPRESS();
 const CORS = require('cors');
 const PATH = require('path');
-const { env } = require('process');
 const LANGUAGES = require('./languages');
 
 const DIST = PATH.join(__dirname, '/client/dist');
-
+const API_URL = 'https://api.cognitive.microsofttranslator.com/translate'
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -25,37 +24,45 @@ app.listen(process.env.PORT || 3000, () => {
   console.log(`APP is running successfully in ${process.env.PORT || 3000}`);
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(EXPRESS.static(DIST));
-  app.get('/rgstay-translator', (req, res) => {
-    res.sendFile(PATH.join(DIST, 'index.html'));
-  })
-}
+// if (process.env.NODE_ENV === 'production') {
+app.use(EXPRESS.static(DIST));
+app.get('/rgstay-translator', (req, res) => {
+  res.sendFile(PATH.join(DIST, 'index.html'));
+})
+// }
 
 
 let formResponseObject = (translatedResponse) => {
   return translatedResponse.reduce((acc, curr, indx) => {
-    LANGUAGES[indx].alias ? acc[LANGUAGES[indx].alias] = curr.text : acc[LANGUAGES[indx].code] = curr.text;
+    acc[LANGUAGES[indx].code] = curr.data[0].translations[0].text;
     return acc;
   }, {});
 };
 
 app.post('/api/translate', async (req, res) => {
-  const TEXT = req.body.textToBeTranslated;
-  const TRANSLATOR = LANGUAGES.map((language) => {
-    return new GOOGLE_TRANSLATOR({
-      from: DEFAULT_LANGUAGE.code,
-      to: language.code
-    }).translate(TEXT);
-
-  });
+  let text = req.body.textToBeTranslated;
+  const TRANSLATOR = LANGUAGES.map(language => {
+    return axios.post(
+      API_URL,
+      [{ text }],
+      {
+        params: {
+          'api-version': '3.0',
+          'from': DEFAULT_LANGUAGE.code,
+          'to': language.code
+        },
+        headers: {
+          'Ocp-Apim-Subscription-Key': process.env.TRANSLATOR_KEY,
+          'Content-type': 'application/json'
+        }
+      }
+    )
+  })
   try {
     let responses = await Promise.all(TRANSLATOR);
-    res.send({ ...{ [DEFAULT_LANGUAGE.code]: TEXT }, ...formResponseObject(responses) });
+    res.send({ ...{ [DEFAULT_LANGUAGE.code]: text }, ...formResponseObject(responses) });
   } catch (error) {
     console.error(error);
   }
 });
-
-
 
